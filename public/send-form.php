f0f6ae41ab9d2e5f251894851609968d9938fabf
andrsweb <proflinervk@gmail.com>
2023-02-11 21:46:48 +0300

    some fix

--- public/send-form.php
+++ public/send-form.php
@@ -245,7 +245,7 @@ function as_send_email( string $subject, string $message ){
 		"Reply-To: no-reply@" . $_SERVER['HTTP_HOST'] . "\r\n" .
 		"X-Mailer: PHP/" . phpversion();
 
-	$result = mail('dr.nalgiev@yandex.ru', $subject, $message, $headers );
+	$result = mail('test@mail.com', $subject, $message, $headers );
 
 	if( $result )
 		echo json_encode( [
