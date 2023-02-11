2edf7b93497f09a3ebb9e5be159a06d4c3186bce
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-02-11 21:15:23 +0300

    fix: submit forms functionality.

---
+++ public/send-form.php
@@ -0,0 +1,263 @@
+<?php
+
+/**
+ * Clean incoming value from trash.
+ *
+ * @param	mixed	$value	Some value to clean.
+ * @return	mixed	$value	The same value, but cleaned.
+ */
+function as_clean_value( $value )
+{
+	$value = trim( $value );
+	$value = stripslashes( $value );
+	$value = strip_tags( $value );
+
+	return htmlspecialchars( $value );
+}
+
+/**
+ * Function checks if value length is between min and max parameters.
+ *
+ * @param   string	$value  Specific string..
+ * @param   int		$min    Minimum symbols value length.
+ * @param   int		$max	Maximum symbols value length.
+ * @return  bool            True if OK, false if value length is too small or large.
+ */
+function as_check_length( string $value, int $min, int $max ): bool
+{
+	return ! ( mb_strlen( $value ) < $min || mb_strlen( $value ) > $max );
+}
+
+/**
+ * Function checks name symbols.
+ *
+ * @param   string  $name   Some name.
+ * @return  bool            True if OK, false if string has bad symbols.
+ */
+function as_check_name( string $name ): bool
+{
+	return preg_match('/^[a-zа-я\s]+$/iu', $name );
+}
+
+/**
+ * Function checks phone symbols.
+ *
+ * @param   string  $phone  Some phone number.
+ * @return  bool            True if OK, false if string has bad symbols.
+ */
+function as_check_phone( string $phone ): bool
+{
+	return preg_match('/^[0-9()+\-\s]+$/iu', $phone );
+}
+
+if( ! empty( $_POST ) && isset( $_POST['func'] ) ){
+	switch( $_POST['func'] ){
+		case 'feedback':
+			as_send_feedback_form();
+			break;
+
+		case 'promo':
+			as_send_promo_form();
+			break;
+
+		default:
+			as_send_popup_form();
+	}
+}
+
+// Feedback.
+function as_send_feedback_form(){
+	$name		= isset( $_POST['name'] ) ? as_clean_value( $_POST['name'] ) : null;
+	$tel		= isset( $_POST['phone'] ) ? as_clean_value( $_POST['phone'] ) : null;
+	$question	= isset( $_POST['question'] ) ? as_clean_value( $_POST['question'] ) : null;
+	$title		= 'Форма обратной связи';
+
+	// Required fields.
+	if( ! $name || ! $tel || ! $question ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, заполните все поля.'
+		] );
+		die();
+	}
+
+	// Only letters & spaces in name.
+	if( ! as_check_name( $name ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, введите корректное имя.'
+		] );
+		die();
+	}
+
+	// Check length to avoid very large text.
+	if( ! as_check_length( $name, 1, 50 ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Имя не должно превышать 50 символов.'
+		] );
+		die();
+	}
+
+	if( ! as_check_length( $tel, 3, 30 ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Телефон не должен превышать 30 символов или быть меньше 3 символов.'
+		] );
+		die();
+	}
+
+	// Check phone symbols.
+	if( ! as_check_phone( $tel ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, введите корректный телефон.'
+		] );
+		die();
+	}
+
+	// Prepare message for mail.
+	$message = "Привет!\n" .
+		"{$title}:\n\n" .
+		"Имя - $name\n" .
+		"Телефон - $tel\n" .
+		"Вопрос - $question\n\n\n";
+
+	as_send_email( $title, $message );
+}
+
+// Promo.
+function as_send_promo_form(){
+	$email	= isset( $_POST['email'] ) ? as_clean_value( $_POST['email'] ) : null;
+	$title	= 'Форма Спецпредложение';
+
+	// Required fields.
+	if( ! $email || ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, заполните все поля.'
+		] );
+		die();
+	}
+
+	if( ! as_check_length( $email, 5, 50 ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Email не должен превышать 50 символов или быть меньше 5 символов.'
+		] );
+		die();
+	}
+
+	// Prepare message for mail.
+	$message = "Привет!\n" .
+		"{$title}:\n\n" .
+		"Email - $email\n\n\n";
+
+	as_send_email( $title, $message );
+}
+
+// Popup.
+function as_send_popup_form(){
+	$name	= isset( $_POST['name'] ) ? as_clean_value( $_POST['name'] ) : null;
+	$email	= isset( $_POST['email'] ) ? as_clean_value( $_POST['email'] ) : null;
+	$tel	= isset( $_POST['phone'] ) ? as_clean_value( $_POST['phone'] ) : null;
+	$title	= 'Форма получения промокода в попапе';
+
+	// Required fields.
+	if( ! $name || ! $email || ! $tel ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, заполните все поля.'
+		] );
+		die();
+	}
+
+	// Only letters & spaces in name.
+	if( ! as_check_name( $name ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, введите корректное имя.'
+		] );
+		die();
+	}
+
+	// Check length to avoid very large text.
+	if( ! as_check_length( $name, 1, 50 ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Имя не должно превышать 50 символов.'
+		] );
+		die();
+	}
+
+	if( ! as_check_length( $tel, 3, 30 ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Телефон не должен превышать 30 символов или быть меньше 3 символов.'
+		] );
+		die();
+	}
+
+	// Check phone symbols.
+	if( ! as_check_phone( $tel ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, введите корректный телефон.'
+		] );
+		die();
+	}
+
+	// Check Email.
+	if( ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Пожалуйста, заполните все поля.'
+		] );
+		die();
+	}
+
+	if( ! as_check_length( $email, 5, 50 ) ){
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Email не должен превышать 50 символов или быть меньше 5 символов.'
+		] );
+		die();
+	}
+
+	// Prepare message for mail.
+	$message = "Привет!\n" .
+		"{$title}:\n\n" .
+		"Имя - $name\n" .
+		"Email - $email\n" .
+		"Телефон - $tel\n\n\n";
+
+	as_send_email( $title, $message );
+}
+
+/**
+ * @param string $subject
+ * @param string $message
+ * @return void
+ */
+function as_send_email( string $subject, string $message ){
+	// Mail headers.
+	$headers = "From: no-reply@" . $_SERVER['HTTP_HOST'] . "\r\n" .
+		"Reply-To: no-reply@" . $_SERVER['HTTP_HOST'] . "\r\n" .
+		"X-Mailer: PHP/" . phpversion();
+
+	$result = mail('dr.nalgiev@yandex.ru', $subject, $message, $headers );
+
+	if( $result )
+		echo json_encode( [
+			'success'	=> 1,
+			'message'	=> 'Спасибо за Ваше сообщение! Мы свяжемся с Вами в ближайшее время.'
+		] );	// Success.
+	else
+		echo json_encode( [
+			'success'	=> 0,
+			'message'	=> 'Ошибка отправки. Пожалуйста, попробуйте позже.'
+		] );	// Failed.
+}
+
+die();
+
--- src/js/common/send-form.js
+++ src/js/common/send-form.js
@@ -13,10 +13,13 @@ const submitForm = ( selector ) => {
 		form.addEventListener( 'submit', e => {
 			e.preventDefault()
 
-			const formResponse	= form.querySelector( '.form-response' ),
-					request		= new XMLHttpRequest(),
-					formData		= new FormData( form )
+			const
+				formResponse	= form.querySelector( '.form-response' ),
+				request			= new XMLHttpRequest(),
+				formData		= new FormData( form ),
+				formType		= form.dataset.type
 
+			formData.append( 'func', formType )
 			request.open( 'post', 'send-form.php', true )
 			request.responseType = 'json'
 
@@ -24,23 +27,25 @@ const submitForm = ( selector ) => {
 			formResponse.textContent = 'Обработка...'
 
 			request.addEventListener( 'load', () => {
+				// All is good.
 				if( request.status === 200 ){
-					
+					// Success response.
 					if( request.response.success ){
 						form.classList.add( 'success' )
 						form.classList.remove( 'error' )
 						form.innerHTML = request.response.message
-					}	else {
+					}	else {	// We've caught an error.
 						formResponse.classList.remove( 'success' )
 						formResponse.classList.add( 'error' )
 						formResponse.textContent = request.response.message
 					}
-				}	else {
+				}	else {	// All is bad.
 					formResponse.classList.remove( 'success' )
 					formResponse.classList.add( 'error' )
 					formResponse.textContent = request.response
 				}
 			} )
+
 			request.send( formData )
 		} )
 	} )
